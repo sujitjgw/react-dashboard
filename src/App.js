cb3a60d412258457f046e7223ba0e06aca6cf9b9
vikrant tyagi <vikrant@progryss.com>
2024-05-29 12:20:14 +0530

    useContext Code Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// src/App.js
-
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Layout from './components/Layout';
@@ -10,19 +8,29 @@ import ListCsvPage from './components/ListCsvPage';
 import LayeredFilterPage from './components/LayeredFilterPage';
 import './components/component_css/Layout.css';
 
+export const CounterContext = createContext();
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/sync" element={<Layout><SyncPage /></Layout>} />
-        <Route path="/upload" element={<Layout><UploadCsvPage /></Layout>} />
-        <Route path="/list" element={<Layout><ListCsvPage /></Layout>} />
-        <Route path="/filter" element={<Layout><LayeredFilterPage/></Layout>} />
-      </Routes>
-    </Router>
+    <CounterContext.Provider value={{ count, increment, setCount }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/sync" element={<Layout><SyncPage /></Layout>} />
+          <Route path="/upload" element={<Layout><UploadCsvPage /></Layout>} />
+          <Route path="/list" element={<Layout><ListCsvPage /></Layout>} />
+          <Route path="/filter" element={<Layout><LayeredFilterPage /></Layout>} />
+        </Routes>
+      </Router>
+    </CounterContext.Provider>
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,12 @@
-// src/components/Dashboard.js
-
-import React from 'react';
+import React, { useContext } from 'react';
+import { CounterContext } from '../App';
 
 export default function Dashboard (){
+  const { count, increment} = useContext(CounterContext);
     return (
         <div>
-          <h2>Dashborad Content</h2>
+          <h2 className='mb-3'>Dashborad Content</h2>
+          <button className='btn btn-danger mb-3' onClick={increment}>Count {count} times</button>
         </div>
       );
 }
